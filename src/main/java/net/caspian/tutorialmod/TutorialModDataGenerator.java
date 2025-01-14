3f7baada11e9c2b44cf785115653a3546dda4601
Nouman <noumanpm@gmail.com>
2025-01-14 23:59:10 +0530

    DataGen and JSON Files

--- src/main/java/net/caspian/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/net/caspian/tutorialmod/TutorialModDataGenerator.java
@@ -1,11 +1,21 @@
 package net.caspian.tutorialmod;
 
+import net.caspian.tutorialmod.datagen.ModBlockTagProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.caspian.tutorialmod.datagen.*;
 
 public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 
 	}
 }
